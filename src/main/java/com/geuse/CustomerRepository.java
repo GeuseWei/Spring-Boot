06760f5c68505726913527ea2096a4e3be847fb6
GeuseWei <122508002+GeuseWei@users.noreply.github.com>
2024-02-03 22:31:24 -0500

    spring boot

---
+++ src/main/java/com/geuse/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.geuse;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+
+    
+} 
---
+++ src/main/java/com/geuse/Main.java
@@ -0,0 +1,80 @@
+package com.geuse;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("api/v1/customers")
+public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+        String name,
+        String email,
+        Integer age
+    ){}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    record UpdateCustomerRequest(
+        String name,
+        String email,
+        Integer age
+    ){}
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer customerId, @RequestBody UpdateCustomerRequest request){
+
+    Customer customer = customerRepository.findById(customerId)
+    .orElseThrow(() -> new RuntimeException("Customer not found")); // 或者使用自定义异常
+
+
+    customer.setName(request.name());
+    customer.setEmail(request.email());
+    customer.setAge(request.age());
+
+
+    customerRepository.save(customer);
+    }
+
+
+}
